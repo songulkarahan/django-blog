5af473678f1d333cca2294e58a55added6245b6e
songulkarahan <songul.karahan@biil.omu.edu.tr>
2020-05-31 04:37:39 +0300

    dinamik detail sayfası düzenlemeleri yapıldı

--- post/views.py
+++ post/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render,HttpResponse
+from .models import Post
 
 # Create your views here.
 
 def post_index(request):
-    return HttpResponse('<b>Post İndex Bebeyimm</b>')
+    posts = Post.objects.all()
+    return render(request, 'post/index.html', {'posts': posts} )
 
-def post_detail(request):
-    return HttpResponse('<b>Post Detail Bebeyimm</b>')
+def post_detail(request, id):
+    post = Post.objects.get(id=id)
+    return render(request, 'post/detail.html', {'post': post})
 
 def post_create(request):
     return HttpResponse('<b>Post Create Bebeyimm</b>')
