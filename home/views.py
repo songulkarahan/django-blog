3fdba3cec39c52e5d8919a3faff66f2497e9eaf2
songulkarahan <songul.karahan@biil.omu.edu.tr>
2020-06-08 05:48:20 +0300

    changed added

--- home/views.py
+++ home/views.py
@@ -3,4 +3,15 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 
 def home_view(request):
-    return HttpResponse('<b>Home sayfası Bebeyim </b>')
+    if request.user.is_authenticated:
+        context = {
+            'isim' : 'Songül',
+            'soyisim': 'Karahan',
+        }
+    else:
+        context = {
+            'isim' : 'Misafir',
+            'soyisim': 'Kullanıcı',
+        }
+
+    return render(request, 'home.html', context)
--- post/urls.py
+++ post/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     url(r'^index/$', post_index),
-    url(r'^detail/$', post_detail),
+    url(r'^(?P<id>\d+)/$', post_detail), #parantez sonrası ?P ifadesi argüman tanımlamak için kullanılır, /d+ ise birden fazla rakam belirteceğini söyler
     url(r'^create/$', post_create),
     url(r'^update/$', post_update),
     url(r'^delete/$', post_delete),
